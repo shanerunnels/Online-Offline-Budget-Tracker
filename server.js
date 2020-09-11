ad16e2027bfdbefb165856af4fbaea1230a8de63
Shane Runnels <shanerunnels@gmail.com>
2020-09-11 18:50:11 -0500

    debug

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/BudgetTracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
